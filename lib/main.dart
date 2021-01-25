d2647ed7f6aa6c3631238f29d409ce5595253ce0
JaiAravindh-git <offl.aravindh@gmail.com>
2021-01-26 00:27:59 +0530

    Arranging items in column before styling

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,65 @@ class HomePage extends StatelessWidget {
         children: [
           Container(
             child: Container(
-              child: Column(
-                children: [],
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          Text("Shriram properties"),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text("Shriram Park 63"),
+                          Text("Perungulathur, Chennai"),
+                          Text("29.30L- 79.79L"),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text("Well Planned integrated townShip"),
+                          Text("20 mins drive to airport"),
+                          Text(
+                              "Right opposite to Perungulathur railway station"),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          FlatButton(
+                            child: Text("I have interested Clients"),
+                            onPressed: () {},
+                          ),
+                          FlatButton(
+                            child: Row(
+                              children: [
+                                Icon(Icons.share_outlined),
+                                Text("Share"),
+                              ],
+                            ),
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                    ],
+                  )),
+                  Container(
+                    child: Column(
+                      children: [
+                        Icon(Icons.access_alarm),
+                        Icon(Icons.access_alarm),
+                        Icon(Icons.access_alarm),
+                        Icon(Icons.access_alarm),
+                        Icon(Icons.access_alarm),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
