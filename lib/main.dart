4aa7c785af0a6fb2870910f905ee38370e50b3bf
JaiAravindh-git <offl.aravindh@gmail.com>
2021-01-27 11:10:23 +0530

    arranged components and added image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -19,66 +21,103 @@ class HomePage extends StatelessWidget {
         scrollDirection: Axis.vertical,
         children: [
           Container(
-            child: Container(
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.ac_unit_outlined),
-                          Text("Shriram properties"),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text("Shriram Park 63"),
-                          Text("Perungulathur, Chennai"),
-                          Text("29.30L- 79.79L"),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text("Well Planned integrated townShip"),
-                          Text("20 mins drive to airport"),
-                          Text(
-                              "Right opposite to Perungulathur railway station"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          FlatButton(
-                            child: Text("I have interested Clients"),
-                            onPressed: () {},
-                          ),
-                          FlatButton(
-                            child: Row(
-                              children: [
-                                Icon(Icons.share_outlined),
-                                Text("Share"),
-                              ],
-                            ),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
-                    ],
-                  )),
-                  Container(
-                    child: Column(
+            child: SafeArea(
+              child: Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Column(
                       children: [
-                        Icon(Icons.access_alarm),
-                        Icon(Icons.access_alarm),
-                        Icon(Icons.access_alarm),
-                        Icon(Icons.access_alarm),
-                        Icon(Icons.access_alarm),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 100, 0),
+                          child: Container(
+                            height: 300.0,
+                            width: 300.0,
+                            child: Image.network(
+                                'https://media.istockphoto.com/photos/modern-apartments-picture-id931243836?k=6&m=931243836&s=612x612&w=0&h=DOa4PvIN9if-FyBTkD-KIbsXUW1GwgSxB1ZItNmsCNA='),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 300, 0, 0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.ac_unit_outlined),
+                              Text(
+                                "Shriram properties",
+                                style: TextStyle(fontSize: 30.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(70, 0, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                "Shriram Park 63",
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                              Text("Perungulathur, Chennai"),
+                              Text(
+                                "29.30L- 79.79L",
+                                style: TextStyle(fontSize: 15.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(90, 0, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text("• Well Planned integrated townShip"),
+                              Text("• 20 mins drive to airport"),
+                              Text(
+                                  "• Right opposite to Perungulathur railway station"),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(80, 20, 20, 0),
+                          child: Row(
+                            children: [
+                              FlatButton(
+                                color: Colors.grey[200],
+                                child: Text("I have interested Clients"),
+                                onPressed: () {},
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              FlatButton(
+                                color: Colors.grey[200],
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.ios_share),
+                                    Text("Share"),
+                                  ],
+                                ),
+                                onPressed: () {},
+                              )
+                            ],
+                          ),
+                        ),
                       ],
+                    )),
+                    Container(
+                      child: Column(
+                        children: [
+                          Icon(Icons.access_alarm),
+                          Icon(Icons.access_alarm),
+                          Icon(Icons.access_alarm),
+                          Icon(Icons.access_alarm),
+                          Icon(Icons.access_alarm),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
